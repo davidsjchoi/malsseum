3be6cf0ece5135cfd4f4a696f566c7a98ff8add1
davidsjchoi <slist.choi@gmail.com>
2026-07-04 15:19:56 +0900

    v0.98: home banner, nav rename, selector cleanup, PWA icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 말씀함께 PWA — 앱 셸 서비스워커 (v0.97)
-const CACHE = 'msh-v0_97-2';
+const CACHE = 'msh-v0_98-1';
 const SHELL = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 

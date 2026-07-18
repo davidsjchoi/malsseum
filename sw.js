05f498184ed4983a1a6735ba15bfbbbf801fcebf
davidsjchoi <slist.choi@gmail.com>
2026-07-19 00:00:40 +0900

    v1.5.1: 카카오 우편번호 주소검색 + 전화 입력란 크기 통일 · sw 캐시 v1_5_1-1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 말씀함께 PWA — 앱 셸 서비스워커 (v1.5)
-const CACHE = 'msh-v1_5-2';
+const CACHE = 'msh-v1_5_1-1';
 const SHELL = ['./', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
